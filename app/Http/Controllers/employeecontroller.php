cec0e5b434627b4691a09b7e40a0462f5209c6c4
Rapidash001 <faldenrichards011@gmail.com>
2025-05-30 10:18:57 +0800

    andami paren wala perowag ka muna

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -11,18 +11,34 @@ class employeecontroller extends Controller
     public function index()
     {   
         $employees = employee::get();
-
-        return view ('employee.index');
+        return view ('employee.index', compact('employees'));
     }
 
-    // employee::create($request->all());
-    // return view ('employee.create');
-    // }
+
+    public function create()
+    {
+    return view ('employee.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request-> validate([
+            'fname' => 'required|max:255|string',
+            'lname' => 'required|max:255|string',
+            'midname' => 'required|max:255|string',
+            'age' => 'required|integer',
+            'address' => 'required|max:255|string',
+            'zip' => 'required|integer',
+        ]);
+        employee::create($request->all());
+        return view ('employee.create');
+    }
 
 
-        
-            // employee::findOrFail($id)->update($request->all());
-            // return redirect ()->back()->with('status','Employee Updated Successfully!');
-            // }
+    public function update(Request $request){
+        //andami ng wala sayo tong dito
+        employee::findOrFail($id)->update($request->all());
+        return redirect ()->back()->with('status','Employee Updated Successfully!');
+    }
 
 }
